ff15007eca0a61f1cb9e2ce735ddfa91cb1ec2ac
guigomes94 <guigomes94@outlook.com>
2019-11-18 23:09:46 -0300

    update18-11-19

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = new Router();
 
-router.get("/", (req, res, next) => {
+router.get("/", (req, res) => {
   res.status(200).send({
     title: "DC Node API",
     version: "0.0.2"
